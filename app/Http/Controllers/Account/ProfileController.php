64b220f15858801533cad98a10044ab3790f5dcf
Marcellino Liemartha <76104601+SolCypher@users.noreply.github.com>
2022-10-19 19:30:36 +0700

    Profile Page (Modified)
    - Change return view from detail -> form
    - Add condition to form.blade.php -> line 54

--- app/Http/Controllers/Account/ProfileController.php
+++ app/Http/Controllers/Account/ProfileController.php
@@ -3,23 +3,34 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\MNPController;
+use App\Helpers\Helper;
 
 class ProfileController extends MNPController
 {
     public function init(){
         $this->title = 'Profile';
         $this->table = 'users';
+
+        $this->forms[] = ['label' => 'Name', 'col' => 'name'];
+        $this->forms[] = ['label' => 'Email', 'col' => 'email'];
+        $this->forms[] = ['label' => 'Password', 'col' => 'email'];
+        $this->forms[] = ['label' => 'Password', 'col' => 'email'];
+
     }
 
-    
     public function getIndex(){
-
+        $this->load();
+        
         if($locale = session('locale')){
             app()->setLocale($locale);
         }
 
-        return view('account.profile', ['title' => 'Profile']);
+        $this->data['page'] = 'details';
+
+        
+        return view('template.form', $this->data);
     }
+
     /*
     public function switch($locale){
         Session::put('locale', $locale);
