0bb9292dac523da20adbed2c4652ebde2c1a678b
nicholascaalvin <nicholas.calvin103@gmail.com>
2022-10-28 17:14:18 +0700

    Update home.blade.php
    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,36 @@
 @extends('layout')
 
+@section('header')
+    <style type="text/css">
+        .card{
+            margin: 10px;
+            padding: 10px;
+        }
+    </style>
+@endsection
+
 @section('content')
-<h1>DASHBOARD</h1>
+
+<div class="card">
+    <div class="card-header">
+        <h1>Dashboard</h1>
+    </div>
+    <div class="card-body">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+    </div>
+</div>
+
 @endsection
