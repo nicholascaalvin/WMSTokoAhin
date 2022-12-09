b6971e72aac85fd0222b5bf87d8871770ed028b6
nicholascaalvin <nicholas.calvin103@gmail.com>
2022-12-09 15:58:23 +0700

    dashboard
    grafik transaction done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -115,7 +115,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex">
         <div class="container-fluid">
-          <a class="navbar-brand" href="{{route('dashboard')}}">WMS</a>
+          <a class="navbar-brand" href="{{route('dashboard')}}" style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif">MNP</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
